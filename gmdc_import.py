86e1a24a61bf897abfda4fbf3b24e605432c1368
Nopke <44393502+Cosmatevs@users.noreply.github.com>
2026-02-08 12:36:51 +0100

    update copyright year

--- gmdc_import.py
+++ gmdc_import.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # Original work: Copyright (C) 2021  DjAlex88 (https://github.com/djalex88/)
-# Modified work: Copyright (C) 2024  Cosmatevs (https://github.com/Cosmatevs/)
+# Modified work: Copyright (C) 2026  Cosmatevs (https://github.com/Cosmatevs/)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
